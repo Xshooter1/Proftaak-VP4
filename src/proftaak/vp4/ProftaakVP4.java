bf7beb0eaf2a90bebff1a48720d8f89d971e6aa6
Dominique Rietveld <rietvelddominique@gmail.com>
2015-05-12 12:56:10 +0200

    base

---
+++ src/proftaak/vp4/ProftaakVP4.java
@@ -0,0 +1,21 @@
+/******************************************************************************\
+**                            Dominique Rietveld                              **
+**                            Student Avans Breda                             **
+\******************************************************************************/
+
+package proftaak.vp4;
+
+/**
+ *
+ * @author Dominique Rietveld
+ */
+public class ProftaakVP4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
