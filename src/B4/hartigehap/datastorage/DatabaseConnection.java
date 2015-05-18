c5047066186366bef5f0ffc7f78825087bf8deea
Dominique Rietveld <rietvelddominique@gmail.com>
2015-05-18 15:05:23 +0200

    test

---
+++ src/B4/hartigehap/datastorage/DatabaseConnection.java
@@ -0,0 +1,16 @@
+/******************************************************************************\
+**                            Dominique Rietveld                              **
+**                            Student Avans Breda                             **
+\******************************************************************************/
+
+package B4.hartigehap.datastorage;
+
+/**
+ *
+ * @author Dominique Rietveld
+ */
+public class DatabaseConnection {
+    public DatabaseConnection(){
+        System.out.println("Ik ben master");
+    }
+}
