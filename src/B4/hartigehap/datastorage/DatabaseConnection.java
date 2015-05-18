36b6f1367f13bad7e07367b4077bb4cd33ab4e3f
Dominique Rietveld <rietvelddominique@gmail.com>
2015-05-18 15:17:08 +0200

    a

--- src/B4/hartigehap/datastorage/DatabaseConnection.java
+++ src/B4/hartigehap/datastorage/DatabaseConnection.java
@@ -10,7 +10,7 @@ package B4.hartigehap.datastorage;
  * @author Dominique Rietveld
  */
 public class DatabaseConnection {
-    
+
     public DatabaseConnection(){
     
     System.out.println("Ik ben database");
