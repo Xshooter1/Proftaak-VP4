f0e9456610f92e118b059aab6ad4cf9dd7ca6d46
Dominique Rietveld <rietvelddominique@gmail.com>
2015-05-18 15:14:28 +0200

    Merge branch 'database'
    Conflicts:
    	src/B4/hartigehap/datastorage/DatabaseConnection.java

--- src/B4/hartigehap/datastorage/DatabaseConnection.java
+++ src/B4/hartigehap/datastorage/DatabaseConnection.java
@@ -10,7 +10,6 @@ package B4.hartigehap.datastorage;
  * @author Dominique Rietveld
  */
 public class DatabaseConnection {
-    public DatabaseConnection(){
-        System.out.println("Ik ben master");
+
     }
 }
