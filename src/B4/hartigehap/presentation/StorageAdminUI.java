f300cfdb78fa859cc7c62113b223329f5d00271c
John Boogaard <johnie96@hotmail.nl>
2015-05-18 15:42:55 +0200

    UI1
    UI bestand aangemaakt

---
+++ src/B4/hartigehap/presentation/StorageAdminUI.java
@@ -0,0 +1,19 @@
+package B4.hartigehap.presentation;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author John Boogaard
+ */
+public class StorageAdminUI extends javax.swing.JFrame {
+    private JLabel labelLowStock;
+    
+    public StorageAdminUI() {
+        setupFrame();
+    }
+    
+    private void setupFrame() {
+    setTitle("Member administration");
+}
+}
